e44c64a79834d1f2dc6e4c656cc7a3af7329cdd9
t-kelly <thomas.kelly@shopify.com>
2021-06-15 22:45:13 -0400

    Add some Polaris components

--- components/PolarisProvider.js
+++ components/PolarisProvider.js
@@ -1,6 +1,29 @@
 import { AppProvider } from "@shopify/polaris";
+import Link from 'next/link'
 import translations from "@shopify/polaris/locales/en.json";
 
+const CustomLinkComponent = ({
+  as,
+  children,
+  url,
+  external,
+  role,
+  ...rest
+}) => {
+  if (external) {
+    return (
+      <a href={url} target="_blank" rel="noopener noreferrer" {...rest}>
+        {children}
+      </a>
+    );
+  }
+  return (
+    <Link href={url}>
+      <button {...rest}>{children}</button>
+    </Link>
+  );
+};
+
 export default function PolarisProvider({children}) {
-  return <AppProvider i18n={translations}>{children}</AppProvider>
+  return <AppProvider i18n={translations} linkComponent={CustomLinkComponent}>{children}</AppProvider>
 }
--- pages/embedded/index.js
+++ pages/embedded/index.js
@@ -1,11 +1,50 @@
-import { Heading, Page } from "@shopify/polaris";
-import Link from 'next/link'
+import { Layout,
+  Page,
+  FooterHelp,
+  Link,
+  MediaCard } from "@shopify/polaris";
+import {} from '@shopify/polaris'
 
 export default function Index() {
+  const primaryAction = {content: 'Settings', url: '/embedded/settings'};
+
   return (
-      <Page>
-        <Heading>Shopify app with Node and React 🎉</Heading>
-        <Link href={`/embedded/page1`}><a>Go to another page</a></Link>
-      </Page>
-  )
+    <Page
+      title="NextJS Shopify App"
+      primaryAction={primaryAction}
+    >
+      <Layout>
+        <Layout.Section>
+          <MediaCard
+            title="Welcome to the Shopify NextJS App!"
+            primaryAction={{
+              content: 'Learn about getting started',
+              url: 'https://shopify.dev/concepts/apps',
+              external: true
+            }}
+            description="It looks like things are setup correctly and you should be able to start developing."
+            popoverActions={[{content: 'Dismiss', onAction: () => {}}]}
+          >
+            <img
+              alt=""
+              width="100%"
+              height="100%"
+              style={{
+                objectFit: 'cover',
+                objectPosition: 'center',
+              }}
+              src="https://burst.shopifycdn.com/photos/business-woman-smiling-in-office.jpg?width=1850"
+            />
+          </MediaCard>
+        </Layout.Section>
+
+        <Layout.Section>
+          <FooterHelp>
+            For more details on Polaris, visit our{' '}
+            <Link url="https://polaris.shopify.com" external={true}>style guide</Link>.
+          </FooterHelp>
+        </Layout.Section>
+      </Layout>
+    </Page>
+  );
 };
